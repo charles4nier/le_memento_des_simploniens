d5c942cdf9f92f5544ca5bc1816abb537a563a1f
charles4nier <charles.4nier@gmail.com>
2017-04-26 10:52:17 +0200

    Cinquième Version

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Version 4
+# Version 5
 
-Tous les différents point de navigation sont créés. Nous pouvons accéder aux articles depuis le header, la nav à gauche du main ou depuis les articles eux m-êmes. Il faut maintenant régler le filtrage de l'affichage dans le menu du header.
+Les problèmes de navigation sont résolus. Il est possible, depuis la navigation du header, d'afficher les articles selon la thèmatique voulu (HTML/CSS/JS/PHP).
+
+Le menu de navigation qui se trouve dans le main recense tous les articles créés.
+
+La navigation avec les boutons "before" et "after" permet de faire dérouler les articles les uns après les autres.
--- view/js/controller.js
+++ view/js/controller.js
@@ -4,11 +4,11 @@ app.controller("accessDataBase", function($scope, $http) {
   $http.get("../modele/req.php")
   .then(function(response) {
 
+    // récupération de la base de donnée
     $scope.articles = response.data.articles;
 
+    // création de length, qui me permet de retrouver l'index de la page voulu (par défaut, le dernier article du blog sera affiché).
     $scope.length = $scope.articles.length - 1;
-
-    // affichage des boutons
 
     // navigation dans le blog avec les boutons before et after
     $scope.before = function() {
@@ -24,10 +24,18 @@ app.controller("accessDataBase", function($scope, $http) {
       }
     }
 
-    $scope.getIndex = function($event, $name) {
-      let index = $event.currentTarget.getAttribute('id');
-      index = index.replace($name, '');
-      $scope.length = index;
+    // navigation depuis les différentes menu de navigation
+    $scope.getIndex = function ($event) {
+      // récupération du titre de l'article au click
+      let title = $event.currentTarget.textContent;
+
+      //comparaison du titre avec les titres contenu dans mon tableau "articles". Une fois qu'un titre correspond, on récupère la valeur de i et on l'affect à scope.length, qui me permet d'afficher la page concernée.
+      for(let i = 0; i < $scope.articles.length; i++) {
+        if($scope.articles[i].title == title) {
+          $scope.length = i;
+        }
+      }
     }
+
   });
 });
