ac3a5b1310ea3fabefb3b2e7afe0101920d89b82
charles4nier <charles.4nier@gmail.com>
2017-08-18 11:32:02 +0200

    ajout des annotations

--- model/crudArticles.php
+++ model/crudArticles.php
@@ -1,72 +1,72 @@
 <?php
-require "sqlReq.php";
+  require "sqlReq.php";
 
-      /**
-       * Récupère tous les articles de la table "article" dans la base de données * * portfolio_db.
-       *
-       * @return array Un tableau contenant toutes les lignes de la table ciblée
-       */
-function getArticles () {
-  global $bdd;
+  /**
+   * getArticles recovers all articles in the "article" table
+   *
+   * @return array An array including all the data coming from the targeted table
+   */
+  function getArticles () {
+    global $bdd;
 
-  $db = $bdd->query('SELECT * FROM article');
-  return $db->fetchAll();
-}
+    $db = $bdd->query('SELECT * FROM article');
+    return $db->fetchAll();
+  }
 
-        /**
-         * Efface un article de la table article
-         *
-         * @param number $id L'identifiant d'une entrée
-         */
-function deleteArticle ($id) {
-  global $bdd;
+  /**
+   * deleteArticle deletes an article from the targeted table
+   *
+   * @param number $id : Input article's id
+   */
+  function deleteArticle ($id) {
+    global $bdd;
 
-  $req =  $bdd->prepare('DELETE FROM article WHERE id = :id');
+    $req =  $bdd->prepare('DELETE FROM article WHERE id = :id');
 
-  $req->execute(array(
-    'id' => $id
-  ));
-}
+    $req->execute(array(
+      'id' => $id
+    ));
+  }
 
-        /**
-         * Créer un article dans la table "article"
-         *
-         * @param number $id L'identifiant d'une entrée
-         * @param string $type Le type de contenu caractérisé par le langage attendu (Js, Php, Css, Html)
-         * @param string $content Le contenu de l'article
-         * @param string $demo Tout contenu ayant pour but d'expliciter le contenu de l'article
-         */
-function createArticle ($title, $type, $content, $demo) {
-  global $bdd;
+  /**
+   * createArticle creates and stores an article into the targeted table
+   *
+   * @param number $id : Input article's id
+   * @param string $type : Typifys the content (Js, Php, Css, Html)
+   * @param string $content : The article content
+   * @param string $demo : A way to explain the article's content
+   */
+  function createArticle ($title, $type, $content, $demo) {
+    global $bdd;
 
-  $req =  $bdd->prepare('INSERT INTO article (title, type, content, demo) VALUES (:title, :type, :content, :demo)');
+    $req =  $bdd->prepare('INSERT INTO article (title, type, content, demo) VALUES (:title, :type, :content, :demo)');
 
-  $req->execute(array(
-    'title' => $title,
-    'type' => $type,
-    'content' => $content,
-    'demo' => $demo
-  ));
-}
+    $req->execute(array(
+      'title' => $title,
+      'type' => $type,
+      'content' => $content,
+      'demo' => $demo
+    ));
+  }
 
-        /**
-         * Edit un article dans la table "article"
-         *
-         * @param number $id L'identifiant d'une entrée
-         * @param string $type Le type de contenu caractérisé par le langage attendu (Js, Php, Css, Html)
-         * @param string $content Le contenu de l'article
-         * @param string $demo Tout contenu ayant pour but d'expliciter le contenu de l'article
-         */
-function editArticle ($type, $title, $content, $demo, $id) {
-  global $bdd;
+  /**
+   * editArticle edits an article from the targeted table
+   *
+   * @param number $id : Input article's id
+   * @param string $type : Typifys the content (Js, Php, Css, Html)
+   * @param string $content : The article content
+   * @param string $demo : A way to explain the article's content
+   */
+  function editArticle ($type, $title, $content, $demo, $id) {
+    global $bdd;
 
-  $req = $bdd->prepare('UPDATE article SET type = :nvxType, title = :nvxTitle, content = :nvxContent, demo = :nvxDemo WHERE id = :id');
+    $req = $bdd->prepare('UPDATE article SET type = :nvxType, title = :nvxTitle, content = :nvxContent, demo = :nvxDemo WHERE id = :id');
 
-  $req->execute(array(
-    'nvxType' => $type,
-    'nvxTitle' => $title,
-    'nvxContent' => $content,
-    'nvxDemo' => $demo,
-    'id' => $id
-  ));
-}
+    $req->execute(array(
+      'nvxType' => $type,
+      'nvxTitle' => $title,
+      'nvxContent' => $content,
+      'nvxDemo' => $demo,
+      'id' => $id
+    ));
+  }
